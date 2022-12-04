eb3f959e3a7317526fd5e2abe6e67bdaad49636a
Shubham Mishra <sh277446@dal.ca>
2022-12-04 11:36:53 -0400

    Add properties file for db credentials

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,6 @@
 public final class Constants {
     public static final String JDBC = "com.mysql.cj.jdbc.Driver";
-    public static final String PROPERTY_FILENAME = "db/power.prop";
+    public static final String PROPERTY_FILENAME = "power.prop";
     public static final String DATABASE_PATH = "path";
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
